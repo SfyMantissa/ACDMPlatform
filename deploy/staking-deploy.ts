64db60af700c942014a1b022b6333cb5442e6fe5
Sfy Mantissa <sfymantissa@protonmail.com>
2022-06-25 05:37:31 +0300

    Added deployment scripts.

---
+++ deploy/staking-deploy.ts
@@ -0,0 +1,14 @@
+import "@nomiclabs/hardhat-ethers";
+import { deploy } from '../utils/deploy-utils';
+import config from "../config";
+
+deploy(
+  "Staking",
+  config.LIQUIDITY_TOKEN_ADDRESS,
+  config.XXXTOKEN_ADDRESS,
+  config.REWARD_PERCENTAGE,
+  config.REWARD_INTERVAL
+).catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ deploy/xxx-token-deploy.ts
@@ -0,0 +1,8 @@
+import "@nomiclabs/hardhat-ethers";
+import { deploy } from '../utils/deploy-utils';
+
+deploy("XXXToken", "0x83695063361619BF2765D885C51Cc6B72D650515").catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
