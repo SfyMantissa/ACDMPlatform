2e8fce5e57251458900eb17fa228c1dcd08d5d46
Sfy Mantissa <sfymantissa@protonmail.com>
2022-06-25 05:34:13 +0300

    Added external ABIs and uttils.

---
+++ utils/deploy-utils.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+import { ethers } from "hardhat";
+
+export const deploy = async (name: string, ...args: any[]) => {
+  const Contract = await ethers.getContractFactory(name);
+  const contract = await Contract.deploy(...args);
+  await contract.deployed();
+  console.log(name + " deployed to:", contract.address);
+  _updateDeploymentAddress(contract.address, name.toUpperCase());
+};
+
+export const testDeploy = async (name: string, ...args: any[]) => {
+  const Contract = await ethers.getContractFactory(name);
+  const contract = await Contract.deploy(...args);
+  await contract.deployed();
+
+  return contract;
+};
+
+const _updateDeploymentAddress = async (address: string, name: string) => {
+  let config: string = './config.ts';
+  fs.readFile(config, 'utf-8', (err: unknown, data: string) => {
+    if (err) throw err;
+    let regexString = name + '_ADDRESS: ".*",';
+    let regex = new RegExp(regexString, "g");
+    let update = data.replace(
+      regex,
+      name + '_ADDRESS: "' + address + '",'
+    );
+
+    fs.writeFile(config, update, 'utf-8', (err: unknown) => {
+      if (err) throw err;
+      console.log('Updated ' + name + '_ADDRESS in config.ts.');
+    });
+  });
+};
---
+++ utils/staking-utils.ts
@@ -0,0 +1,67 @@
+import ERC20Token from "../abi/erc20_abi.json";
+import IUniswapV2Factory from '../abi/factory_abi.json';
+import IUniswapV2Router02 from "../abi/router02_abi.json";
+import IUniswapV2Pair from '../abi/pair_abi.json';
+
+import { Contract } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import { ethers } from "hardhat";
+import config from "../config";
+
+export const addLiquidity = async (
+  staking: Contract,
+  signer: SignerWithAddress,
+  provideAmount: number,
+  ltAmount: number
+) => {
+  let afterMinute = new Date().getTime() + 60;
+
+  const xxxToken = new ethers.Contract(
+    config.XXXTOKEN_ADDRESS,
+    ERC20Token.abi,
+    signer
+  );
+
+  await xxxToken.connect(signer).mint(signer.address, provideAmount);
+  await xxxToken
+    .connect(signer)
+    .approve(config.ROUTER02_ADDRESS, provideAmount);
+
+  const router02 = new ethers.Contract(
+    config.ROUTER02_ADDRESS,
+    IUniswapV2Router02.abi,
+    signer
+  );
+
+  const factory = new ethers.Contract(
+    config.FACTORY_ADDRESS,
+    IUniswapV2Factory.abi,
+    signer
+  );
+
+  await router02
+    .connect(signer)
+    .addLiquidityETH(
+      config.XXXTOKEN_ADDRESS,
+      provideAmount,
+      provideAmount,
+      provideAmount,
+      signer.address,
+      afterMinute,
+      { value: ethers.utils.parseUnits(provideAmount.toString(), 13) }
+    );
+
+  const liquidityTokenAddress = await factory.getPair(
+    config.XXXTOKEN_ADDRESS,
+    await router02.WETH()
+  );
+
+  const liquidityToken = new ethers.Contract(
+    liquidityTokenAddress,
+    IUniswapV2Pair.abi,
+    signer
+  );
+
+  await liquidityToken.connect(signer).approve(staking.address, ltAmount);
+};
