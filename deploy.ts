2a5115048586f681d74a4e6a83865dff6135ad37
Sfy Mantissa <sfymantissa@protonmail.com>
2022-07-03 23:24:03 +0300

    Updated deployment scripts.

--- deploy.ts
+++ deploy.ts
@@ -1,23 +1,26 @@
 import "@nomiclabs/hardhat-ethers";
-import { deploy, _readDeploymentData, _writeDeploymentData } from './utils/deploy-utils';
+import { deploy, readDeploymentData, writeDeploymentData } from './utils/deploy-utils';
 
-let acdmToken, xxxToken, staking, dao;
-let data = _readDeploymentData();
+let data = readDeploymentData();
 
-const deployChain = async (data: any[]) => {
-  acdmToken = await deploy("ACDMToken");
-  xxxToken = await deploy("XXXToken");
-
-  data["Staking"].args[2] = xxxToken.address;
-  staking = await deploy("Staking");
-
-  data["DAOVoting"].args[1] = staking.address;
-  dao = await deploy("DAOVoting");
-
+deploy("ACDMToken").then((acdmToken) => {
+  data["ACDMToken"].address = acdmToken.address;
   data["ACDMPlatform"].args[2] = acdmToken.address;
-  data["ACDMPlatform"].args[3] = xxxToken.address;
-  data["ACDMPlatform"].args[4] = dao.address;
-  await deploy("ACDMPlatform");
-}
-
-deployChain(data).then(() => _writeDeploymentData(data));
+  deploy("XXXToken").then((xxxToken) => {
+    data["XXXToken"].address = xxxToken.address;
+    data["Staking"].args[2] = xxxToken.address;
+    data["ACDMPlatform"].args[3] = xxxToken.address;
+    deploy("Staking").then((staking) => {
+      data["Staking"].address = staking.address;
+      data["DAOVoting"].args[1] = staking.address;
+      deploy("DAOVoting").then((daoVoting) => {
+        data["DAOVoting"].address = daoVoting.address;
+        data["ACDMPlatform"].args[4] = daoVoting.address;
+        deploy("ACDMPlatform").then((acdmPlatform) => {
+          data["ACDMPlatform"].address = acdmPlatform.address;
+          writeDeploymentData(data);
+        });
+      });
+    });
+  });
+});
--- utils/deploy-utils.ts
+++ utils/deploy-utils.ts
@@ -11,39 +11,29 @@ export const testDeploy = async (name: string, ...args: any[]) => {
   return contract;
 };
 
-export const verifyDeploy = async (name: string) => {
-  const data = _readDeploymentData();
+export const deploy = async (name: string) => {
+  const data = readDeploymentData();
   const args = data[name].args;
-
   const contract = await testDeploy(name, ...args ?? "");
-
   console.log(name + " deployed to:", contract.address);
-  data[name].address = contract.address;
-  _writeDeploymentData(data);
+
+  return contract;
+};
+
+export const verifyDeploy = async (name: string) => {
+  const contract = await deploy(name);
+  const data = readDeploymentData();
   _verifyDeployment(data[name]);
 
   return contract;
 };
 
-export const deploy = async (name: string) => {
-  const data = _readDeploymentData();
-  const args = data[name].args;
-
-  const contract = await testDeploy(name, ...args ?? "");
-
-  console.log(name + " deployed to:", contract.address);
-  data[name].address = contract.address;
-  _writeDeploymentData(data);
-
-  return contract;
-};
-
-export const _readDeploymentData = () => {
+export const readDeploymentData = () => {
   const rawData: any = fs.readFileSync(CONFIG);
   return JSON.parse(rawData);
 };
 
-export const _writeDeploymentData = (data: string) => {
+export const writeDeploymentData = (data: string) => {
   const rawData = JSON.stringify(data, null, 2);
   fs.writeFileSync(CONFIG, rawData);
 };
